453c10ed403128f9184033d9ab724265a592f0b7
youssefmys <mosa500@gmail.com>
2017-04-20 06:12:25 +0200

    adding importing function to drug.rb

--- lib/drug.rb
+++ lib/drug.rb
@@ -23,5 +23,13 @@ class Drug
     end
   end
 
+  def self.import_drugs_from_array(drugs_array)
+
+    #getting info from scrapper array of hashes
+    drugs_array.each do |drug_hash|
+      drug = Drug.new(drug_hash) # using hash of attributes from scrapper
+      @@all << drug unless @@all.any? {|d| d.name == drug_hash[:name]}
+    end
+  end
   
 end
