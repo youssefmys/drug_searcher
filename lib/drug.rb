f4b42eeb2639a3f5a6d53791f903cfd6cb0c46a8
youssefmys <mosa500@gmail.com>
2017-04-20 05:39:15 +0200

    adding finders to drug.rb and adding README.md file

---
+++ lib/drug.rb
@@ -0,0 +1,27 @@
+class Drug
+  attr_accessor :name  :website
+
+  @@all = []
+  
+  def initialize(attributes)
+    attributes.each do |key, value|
+      self.send("#{key}=", value)
+    end
+  end
+
+  #writing finders
+
+  def self.find_drug_by_name(name)
+    #search if drug name contains the search string
+    found_drugs = @@all.find_all {|drug| drug.name.downcase.include? (name.downcase)}
+    found_drugs.empty? ? nil : found_drugs
+  end
+
+  def self.find_drugs_by_letter(letter)
+    found_drugs = @all.find_all do |drug|
+      drug.name.start_with?(letter.upcase) || drug.name.start_with?(letter.downcase)
+    end
+  end
+
+  
+end
